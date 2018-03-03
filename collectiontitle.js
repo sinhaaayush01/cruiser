34712ac46c3527ff8984ab493ea9e61ccce04f68
KrnAggarwal <karan.agg143@gmail.com>
2018-03-03 13:26:13 +0530

    Update collectiontitle.js
    changed the add to add1 in last line

--- collectiontitle.js
+++ collectiontitle.js
@@ -1,4 +1,4 @@
-	var a = {};
+	/*var a = {};
 	function setcollectiontitle() {
 	    var title = document.getElementById('collectiontitle').value;
 	    a[title] = [];
@@ -7,4 +7,23 @@
 	    var add1 = `<ul id="${title}" onclick="settitle(document.getElementById('${title}'))"><a href="#!">${title}</a></ul>`;
 	    document.getElementById('coltitle').innerHTML += add;
 	    document.getElementById('bod').innerHTML += add;
+	};
+	
+	*/
+
+	var a = {};
+
+	function setcollectiontitle() {
+
+	    var title = document.getElementById('collectiontitle').value;
+	  
+	    a[title] = [];
+
+	    var add = `<ul id="${title}"><a href="#!">${title}</a></ul>`;
+
+	    var add1 = `<ul id="${title}" onclick="addtocollection1('${title}')"><a href="#!">${title}</a></ul>`;
+
+	    document.getElementById('coltitle').innerHTML += add;
+
+	    document.getElementById('bod').innerHTML += add1;
 	};
