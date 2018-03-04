edb1f13026f1aef8841639f5cb241eaaa73ade0c
KrnAggarwal <karan.agg143@gmail.com>
2018-03-04 13:12:29 +0530

    Update collectiontitle.js
    just changed the  format of passing the title in the delete function just have quotes on the title.

--- collectiontitle.js
+++ collectiontitle.js
@@ -6,7 +6,7 @@ function setcollectiontitle() {
 
     a[title] = [];
 
-    var add = `<ul id="${title}"><a href="#!">${title}</a><button class='btn' onclick='deleteelem(${title})'>Delete</button></ul>`;
+    var add = `<ul id="${title}"><a href="#!">${title}</a><button class='btn' onclick="deleteelem('${title}')">Delete</button></ul>`;
 
     var add1 = `<ul id="${title}" onclick="addtocollection1('${title}')"><a href="#!" data-dismiss="modal">${title}</a></ul>`;
 
